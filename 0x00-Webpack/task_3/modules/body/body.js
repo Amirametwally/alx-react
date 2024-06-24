d9ba2b157100dbf0269e60dac2f8c882942c997d
Amirametwally <amira.metwally56@gmail.com>
2024-06-25 00:11:10 +0300

    3. Dev servers, modules, and tree shaking

--- 0x00-Webpack/task_3/modules/body/body.js
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -1,18 +1,20 @@
-import $ from "jquery";
-import './body.css'
-const _ = require('lodash');
-
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id=\'count\'></p>');
-
-$('button').on('click', _.debounce(updateCounter, 500, {
-  'leading': true,
-  'trailing': false,
-}));
+const $ = require( "jquery" );
+// Load the full build
+const _ = require("lodash");
+import "./body.css";
 
 let count = 0;
 
 function updateCounter() {
-  count = count + 1;
-  $('#count').text(`${count} clicks on the button`);
+  count++;
+  $( "#count" ).text(`${count} clicks on the button` );
 }
+
+const $button = $("<button>Click here to get started</button>").on(
+  "click",
+  _.debounce( updateCounter, 500, { leading: true, trailing: false })
+);
+
+$('body').append("<p>Dashboard data for the students</p>");
+$('body').append($button);
+$('body').append("<p id='count'></p>");
--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,24 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  performance: {
+    maxAssetSize: 1000000,
+    hints: false,
+    maxEntrypointSize: 1000000,
+  },
+  plugins: [ new CleanWebpackPlugin(), new HtmlWebpackPlugin() ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './public'),
+    compress: true,
+    port: 8564,
+  },
   output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.jpg$/,
-        use: {
-          loader: 'file-loader'
-        }
-      }
-    ]
-  }
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
