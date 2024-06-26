14a5092b8bf553f233eccbd1abe8d0bfac46b6e0
Amirametwally <amira.metwally56@gmail.com>
2024-06-26 22:10:02 +0300

    9. Create a project using Webpack

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -1,51 +1,24 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ImageWebpackLoader = require("image-webpack-loader");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "../dist"),
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "../dist"),
-    hot: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: [
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.jpg$/,
         use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-  ],
-  devtool: "inline-source-map",
+          loader: 'image-webpack-loader'
+        }
+      }
+    ]
+  }
 };
