ba9dcc5bb6bf3b8979673ed65654ef70a41cf598
Amirametwally <amira.metwally56@gmail.com>
2024-07-05 00:26:02 +0300

    0. Commence with class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, Component } from "react";
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
@@ -7,33 +7,34 @@ import Footer from "../Footer/Footer";
 import PropTypes from "prop-types";
 import "./App.css";
 
-
 class App extends Component {
   render() {
     const { isLoggedIn } = this.props;
 
     const listCourses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 }
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
     ];
 
     const listNotifications = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
       {
         id: 3,
-        type: 'urgent',
-        html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' }
-      }
+        type: "urgent",
+        html: {
+          __html: "<strong>Urgent requirement</strong> - complete by EOD",
+        },
+      },
     ];
 
     return (
       <Fragment>
         <Notifications listNotifications={listNotifications} />
-        <div className='App'>
+        <div className="App">
           <Header />
-          <div className='App-body'>
+          <div className="App-body">
             {!isLoggedIn && <Login />}
             {isLoggedIn && <CourseList listCourses={listCourses} />}
           </div>
@@ -45,11 +46,11 @@ class App extends Component {
 }
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool
+  isLoggedIn: PropTypes.bool,
 };
 
 App.defaultProps = {
-  isLoggedIn: false
+  isLoggedIn: false,
 };
 
 export default App;
